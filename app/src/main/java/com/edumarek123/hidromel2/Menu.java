183eeea3f10071519aacea5269a212487273fc07
edumarek123 <edumarek123@gmail.com>
2022-08-22 17:07:43 -0300

    Botoes e Telas

--- app/src/main/java/com/edumarek123/hidromel2/Menu.java
+++ app/src/main/java/com/edumarek123/hidromel2/Menu.java
@@ -7,20 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.menu_hidromel);
 
-        Button botao_login=(Button)findViewById(R.id.botao_login);
-        botao_login.setOnClickListener(this::onClick);
+        Button botao_logout=(Button)findViewById(R.id.botao_logout);
+        botao_logout.setOnClickListener(this::onClick);
     }
 
-    public void onClick(View v){
-        Intent intent=new Intent(MainActivity.this, Menu.class);
+    public void onClick(View v) {
+        Intent intent = new Intent(Menu.this, MainActivity.class);
         startActivity(intent);
     }
 }
