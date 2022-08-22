8cf60746c4a1a62098db2c0af243525e14ac8aa9
edumarek123 <edumarek123@gmail.com>
2022-08-22 15:32:40 -0300

    primeiro commit

---
+++ app/src/test/java/Usuario/Usuario.java
@@ -0,0 +1,14 @@
+package Usuario;
+
+import kotlin.text.UStringsKt;
+
+public class Usuario {
+    private long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String permissao;
+    private boolean aceitaTermos;
+
+
+}
