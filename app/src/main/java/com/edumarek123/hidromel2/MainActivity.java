3c98807af384d1b05a2b2b7ae81d651831bf7d69
edumarek123 <edumarek123@gmail.com>
2022-08-29 16:39:05 -0300

    commiting

--- app/src/main/java/com/edumarek123/hidromel2/MainActivity.java
+++ app/src/main/java/com/edumarek123/hidromel2/MainActivity.java
@@ -6,24 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    //atributos
 
+    private EditText tvUsuario;
+    private EditText tvSenha;
+    private Button btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        tvUsuario = (EditText)findViewById(R.id.campo_email_login);
+        tvSenha = (EditText)findViewById(R.id.campo_senha_login);
         Button botao_login=(Button)findViewById(R.id.botao_login);
         botao_login.setOnClickListener(this::onClick);
     }
 
-    public void onClick(View v){
-        
+    public void onClick(View campos_login){
+        if (tvUsuario.getText().toString().equals( "usuario") && tvSenha.getText().toString().equals( "123")){
+            Intent intent=new Intent(MainActivity.this, Menu.class);
+            startActivity(intent);
+        } else {
+            Snackbar mySnackbar = Snackbar.make(campos_login,
+                    "Erro", Snackbar.LENGTH_LONG);
+            mySnackbar.show();
+        }
 
 
-        Intent intent=new Intent(MainActivity.this, Menu.class);
-        startActivity(intent);
+
     }
 }
