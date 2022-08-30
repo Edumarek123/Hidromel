eef9bac7f0465217069313a39a70dccd8c3f3320
edumarek123 <edumarek123@gmail.com>
2022-08-30 11:32:17 -0300

    commiting

--- app/src/main/java/com/edumarek123/hidromel2/MainActivity.java
+++ app/src/main/java/com/edumarek123/hidromel2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.edumarek123.hidromel2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -19,26 +21,31 @@ public class MainActivity extends AppCompatActivity {
     private EditText evSenha;
     private Button btLogin;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         evUsuario = (EditText)findViewById(R.id.campo_email_login);
         evSenha = (EditText)findViewById(R.id.campo_senha_login);
-        Button botao_login=(Button)findViewById(R.id.botao_login);
+        Button botao_login=(Button)findViewById(R.id.button111);
         botao_login.setOnClickListener(this::l_ogar);
     }
 
     public void l_ogar(View v){
-        if (evUsuario.getText().toString().equals( "usuario") && evSenha.getText().toString().equals( "123")){
+
+        /*if (evUsuario.getText().toString().equals("") && evSenha.getText().toString().equals("")){
             Intent intent=new Intent(MainActivity.this, Menu.class);
             startActivity(intent);
         } else {
             Snackbar mySnackbar = Snackbar.make(v, "Erro ao logar", Snackbar.LENGTH_LONG);
             mySnackbar.show();
-        }
+        }*/
 
-
+        LoginFragment login_page=new LoginFragment();
+        FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.loginPage, login_page);
+        transaction.commit();
 
     }
 }
