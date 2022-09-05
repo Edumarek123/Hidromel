1cc15c2b2d2cd3d56c540e0fb9d3fa07e792632c
edumarek123 <edumarek123@gmail.com>
2022-09-05 14:48:27 -0300

    commiting

--- gitUp.sh
+++ gitUp.sh
@@ -2,4 +2,4 @@ git add .
 
 git commit -m "commiting"
 
-git push https://Edumarek123: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@github.com/Edumarek123/Hidromel.git Master
+git push https://Edumarek123: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@github.com/Edumarek123/Hidromel.git Master
