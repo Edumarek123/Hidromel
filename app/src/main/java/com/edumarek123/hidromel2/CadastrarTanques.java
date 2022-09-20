3aca4a459ea5353dd2057c83d96c7f6edd211f62
edumarek123 <edumarek123@gmail.com>
2022-09-19 21:22:45 -0300

    commiting

--- app/src/main/java/com/edumarek123/hidromel2/CadastrarTanques.java
+++ app/src/main/java/com/edumarek123/hidromel2/CadastrarTanques.java
@@ -15,12 +15,15 @@ import android.widget.EditText;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.edumarek123.hidromel2.tanque.Tanque;
 import com.google.android.material.snackbar.Snackbar;
 
-public class CadastrarTanques extends Fragment {
+public class CadastrarTanques extends Fragment implements
+        Response.ErrorListener, Response.Listener{
 
     private EditText tqNome;
     private EditText tqBebida;
@@ -30,10 +33,12 @@ public class CadastrarTanques extends Fragment {
     private RequestQueue requestQueue;
     private JsonObjectRequest jsonObjectReq;
 
+    private View cadastartanque_page;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View cadastartanque_page=inflater.inflate(R.layout.fragment_cadastrar_tanques, container, false);
+        this.cadastartanque_page=inflater.inflate(R.layout.fragment_cadastrar_tanques, container, false);
 
         tqNome = cadastartanque_page.findViewById(R.id.campo_nome_cadastrar_tanque);
         tqBebida = cadastartanque_page.findViewById(R.id.campo_bebida_cadastrar_tanque);
@@ -43,9 +48,10 @@ public class CadastrarTanques extends Fragment {
         Button botao_voltar= cadastartanque_page.findViewById(R.id.botao_voltar);
         botao_voltar.setOnClickListener(this::v_oltar);
 
-        Button botao_cadastrar_tanque= cadastartanque_page.findViewById(R.id.botao_cadastrar_tanque);
-        botao_cadastrar_tanque.setOnClickListener(this::v_oltar);
-
+        Button botao_cadastrar_tanque= cadastartanque_page.findViewById(R.id.botao_cadastrar_tanque1);
+        botao_cadastrar_tanque.setOnClickListener(this::c_adastar);
+        requestQueue = Volley.newRequestQueue(cadastartanque_page.getContext());
+        requestQueue.start();
         // Inflate the layout for this fragment
         return cadastartanque_page;
     }
@@ -58,22 +64,28 @@ public class CadastrarTanques extends Fragment {
     }
 
     public void c_adastar(View v){
-
         Tanque t=new Tanque();
-        t.setNome(tqNome.toString());
-        t.setBebida(tqBebida.toString());
-        t.setEspecificacoes(tqEspecificacoes.toString());
-        t.setVolume(Float.valueOf(tqEspecificacoes.toString()));
+        t.setNome(tqNome.getText().toString());
+        t.setBebida(tqBebida.getText().toString());
+        t.setEspecificacoes(tqEspecificacoes.getText().toString());
+        t.setVolume(12);
 
         jsonObjectReq = new JsonObjectRequest(
                 Request.Method.POST,
-                "http://localhost:8080/hidromel/rest/tanque",
-                t.toJsonObject(), null, null);
+                "http://10.0.2.2:8080/hidromel/rest/tanque",
+                t.toJsonObject(), this, this);
         requestQueue.add(jsonObjectReq);
+    }
 
-        requestQueue = Volley.newRequestQueue(v.getContext());
-        requestQueue.start();
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Snackbar mySnackbar = Snackbar.make( cadastartanque_page,
+                error.toString(), Snackbar.LENGTH_SHORT);
+        mySnackbar.show();
+    }
 
+    @Override
+    public void onResponse(Object response) {
         MenuFragment menu_page=new MenuFragment();
         FragmentManager fragmentManager= getActivity().getSupportFragmentManager();
         FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
